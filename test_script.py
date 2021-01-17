4ae3151d29fd0e3575e5acb85b553c4a32d9f437
visuka <vijaysundeep@gmail.com>
2021-01-17 19:18:01 +0530

    second

--- test_script.py
+++ test_script.py
@@ -4,39 +4,33 @@ from time import sleep
 
 
 class Rover():
-    def __init__(self):
-        self.in1 = 24
-        self.in2 = 23
-        self.en = 25
-        self.l1=17
-        self.r2=27
-        self.en2=22
-        self.temp1=1
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.in1,GPIO.OUT)
-        GPIO.setup(self.in2,GPIO.OUT)
-        GPIO.setup(self.en,GPIO.OUT)
-        GPIO.setup(self.l1,GPIO.OUT)
-        GPIO.setup(self.r2,GPIO.OUT)
-        GPIO.setup(self.en2,GPIO.OUT)
-        GPIO.output(self.in1,GPIO.LOW)
-        GPIO.output(self.in2,GPIO.LOW)
-        GPIO.output(self.l1,GPIO.LOW)
-        GPIO.output(self.r2,GPIO.LOW)
-        p=GPIO.PWM(self.en,1000)
-        p2=GPIO.PWM(self.en2,1000)
+    in1 = 24
+    in2 = 23
+    en = 25
+    l1=17
+    r2=27
+    en2=22
+    temp1=1
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.in1,GPIO.OUT)
+    GPIO.setup(self.in2,GPIO.OUT)
+    GPIO.setup(self.en,GPIO.OUT)
+    GPIO.setup(self.l1,GPIO.OUT)
+    GPIO.setup(self.r2,GPIO.OUT)
+    GPIO.setup(self.en2,GPIO.OUT)
+    GPIO.output(self.in1,GPIO.LOW)
+    GPIO.output(self.in2,GPIO.LOW)
+    GPIO.output(self.l1,GPIO.LOW)
+    GPIO.output(self.r2,GPIO.LOW)
+    p=GPIO.PWM(self.en,1000)
+    p2=GPIO.PWM(self.en2,1000)
         
-        
+           
     def run(self):
-        print("running")
-        while(1):
-            x=input()
-            if x=='r':
-                GPIO.output(self.in1,GPIO.HIGH)
-                GPIO.output(self.in2,GPIO.LOW)
-            else:
-                break
-         
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+    
+        
          
 
 r=Rover()
