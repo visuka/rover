9d9fdab7c2ce954547a82da054737904da214657
visuka <vijaysundeep@gmail.com>
2021-01-17 18:58:28 +0530

    second

--- test_script.py
+++ test_script.py
@@ -13,20 +13,7 @@ class Rover():
         self.en2=22
         self.temp1=1
         self.GPIO=GPIO.setmode(GPIO.BCM)
-        self.GPIO.setup(in1,GPIO.OUT)
-        self.GPIO.setup(in2,GPIO.OUT)
-        self.GPIO.setup(en,GPIO.OUT)
-        self.GPIO.setup(l1,GPIO.OUT)
-        self.GPIO.setup(r2,GPIO.OUT)
-        self.GPIO.setup(en2,GPIO.OUT)
-        self.GPIO.output(in1,GPIO.LOW)
-        self.GPIO.output(in2,GPIO.LOW)
-        self.GPIO.output(l1,GPIO.LOW)
-        self.GPIO.output(r2,GPIO.LOW)
-        self.p=GPIO.PWM(en,1000)
-        self.p2=GPIO.PWM(en2,1000)
-        self.p.start(25)
-        self.p2.start(100)
+        
         
     def run():
         print("running")
